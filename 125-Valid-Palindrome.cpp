6aef090db7b0498443733a41767a980d8f88dfd0
andysheng <andysheng@live.com>
2016-01-06 22:05:04 +0800

    125

---
+++ 125-Valid-Palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if (s == "") return true;
+        int i = 0, j = s.length() - 1;
+        bool isLetter;
+        while (i < j) {
+            while(i < j && !isalnum(s[i])) i++;
+            while(i < j && !isalnum(s[j]))  j--;
+            if (tolower(s[i]) != tolower(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
