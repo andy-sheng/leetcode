4ae914aea54e24093ec5243b390d80063b43bf60
Andy Sheng <andysheng@bupt.edu.cn>
2017-01-24 17:06:22 +0800

    Update 492. Construct the Rectangle.java

--- 492. Construct the Rectangle.java
+++ 492. Construct the Rectangle.java
@@ -1,18 +1,7 @@
 public class Solution {
     public int[] constructRectangle(int area) {
-        int[] result = new int[2];
-        int difference = Integer.MAX_VALUE;
-        for(int w = 1; w <= Math.sqrt(area); ++w) {
-            if (area % w == 0) {
-                int l = area / w;
-                if (l - w <= difference) {
-                    difference = l - w;
-                    result[0] = l;
-                    result[1] = w;
-                }
-            }
-        }
-
-        return result; 
+        int w = (int)Math.sqrt(area);
+        while(area%w != 0) --w;
+        return new int[]{area/w, w};
     }
 }
