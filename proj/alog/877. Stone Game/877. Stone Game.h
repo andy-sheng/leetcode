c8824343969386aa7a2ac8e2520ed5110ff30c78
andysheng <andysheng@tencent.com>
2019-08-12 23:25:33 +0800

    add

---
+++ proj/alog/877. Stone Game/877. Stone Game.h
@@ -0,0 +1,21 @@
+//
+//  877. Stone Game.h
+//  leetcode
+//
+//  Created by andysheng on 2019/8/12.
+//  Copyright © 2019 盛旭辉. All rights reserved.
+//
+
+#ifndef _77__Stone_Game_h
+#define _77__Stone_Game_h
+
+namespace StoneGame {
+    class Solution1 {
+    public:
+        bool stoneGame(vector<int>& piles) {
+            return true;
+        }
+    };
+}
+
+#endif /* _77__Stone_Game_h */
