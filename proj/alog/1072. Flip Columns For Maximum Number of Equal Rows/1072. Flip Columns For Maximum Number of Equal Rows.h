f508abc98514cec7d4971d3b16fea499f5c166a6
andysheng <andysheng@live.com>
2019-11-12 01:00:03 +0800

    1072

---
+++ proj/alog/1072. Flip Columns For Maximum Number of Equal Rows/1072. Flip Columns For Maximum Number of Equal Rows.h
@@ -0,0 +1,13 @@
+//
+//  1072. Flip Columns For Maximum Number of Equal Rows.h
+//  leetcode
+//
+//  Created by andysheng on 2019/11/11.
+//  Copyright © 2019 Andy. All rights reserved.
+//
+
+#ifndef _072__Flip_Columns_For_Maximum_Number_of_Equal_Rows_h
+#define _072__Flip_Columns_For_Maximum_Number_of_Equal_Rows_h
+
+
+#endif /* _072__Flip_Columns_For_Maximum_Number_of_Equal_Rows_h */
